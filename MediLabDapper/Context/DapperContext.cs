42b6347e480cb747b777740e6dab072a01f45256
Koray Vatansever <koray-vatansever@hotmail.com>
2025-04-03 22:38:13 +0300

    Add project files.

---
+++ MediLabDapper/Context/DapperContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace MediLabDapper.Context
+{
+    public class DapperContext
+    {
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("SqlConnection");
+        }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
