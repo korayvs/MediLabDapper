e14ef45539cba2528acaf5c5a89a7865a9273b69
Koray Vatansever <koray-vatansever@hotmail.com>
2025-04-04 22:07:54 +0300

    43.2 update

---
+++ MediLabDapper/Repositories/DoctorRepositories/DoctorRepository.cs
@@ -0,0 +1,54 @@
+﻿using Dapper;
+using MediLabDapper.Context;
+using MediLabDapper.Dtos.DoctorDtos;
+using System.Data;
+using System.Runtime.InteropServices;
+
+namespace MediLabDapper.Repositories.DoctorRepositories
+{
+    public class DoctorRepository(DapperContext _context) : IDoctorRepository
+    {
+        private readonly IDbConnection _db = _context.CreateConnection();
+        public async Task CreateDoctorAsync(CreateDoctorDto createDoctorDto)
+        {
+            var query = "insert into doctors (namesurname, ImageUrl, description, departmentId) values (@NameSurname, @ImageUrl, @Description, @DepartmentId)";
+            var parameters = new DynamicParameters(createDoctorDto);
+            await _db.ExecuteAsync(query, parameters);
+        }
+
+        public async Task DeleteDoctorAsync(int id)
+        {
+            var query = "delete from doctors where DoctorId = @DoctorId";
+            var parameters = new DynamicParameters();
+            parameters.Add("@DoctorId", id);
+            await _db.ExecuteAsync(query, parameters);
+        }
+
+        public async Task<IEnumerable<ResultDoctorDto>> GetAllDoctorsAsync()
+        {
+            var query = "select * from doctors";
+            return await _db.QueryAsync<ResultDoctorDto>(query);
+        }
+
+        public async Task<GetDoctorByIdDto> GetDoctorByIdAsync(int id)
+        {
+            var query = "select * from doctors where DoctorId = @DoctorId";
+            var parameters = new DynamicParameters();
+            parameters.Add("@DoctorId", id);
+            return await _db.QueryFirstOrDefaultAsync<GetDoctorByIdDto>(query, parameters);
+        }
+
+        public async Task<IEnumerable<ResultDoctorWithDepartmentDto>> GetDoctorsWithDepartmentAsync()
+        {
+            var query = "select DoctorId, NameSurname, ImageUrl, Doctors.Description, DepartmentName from doctors inner join departments on Doctors.DepartmentId = Departments.DepartmentId";
+            return await _db.QueryAsync<ResultDoctorWithDepartmentDto>(query);
+        }
+
+        public async Task UpdateDoctorAsync(UpdateDoctorDto updateDoctorDto)
+        {
+            var query = "update doctors set NameSurname = @NameSurname, ImageUrl = @ImageUrl, Description = @Description, DepartmentId = @DepartmentId where DoctorId = @DoctorId";
+            var parameters = new DynamicParameters(updateDoctorDto);
+            await _db.ExecuteAsync(query, parameters);
+        }
+    }
+}
---
+++ MediLabDapper/Repositories/DoctorRepositories/IDoctorRepository.cs
@@ -0,0 +1,14 @@
+﻿using MediLabDapper.Dtos.DoctorDtos;
+
+namespace MediLabDapper.Repositories.DoctorRepositories
+{
+    public interface IDoctorRepository
+    {
+        Task<IEnumerable<ResultDoctorDto>> GetAllDoctorsAsync();
+        Task<IEnumerable<ResultDoctorWithDepartmentDto>> GetDoctorsWithDepartmentAsync();
+        Task<GetDoctorByIdDto> GetDoctorByIdAsync(int id);
+        Task CreateDoctorAsync(CreateDoctorDto createDoctorDto);
+        Task UpdateDoctorAsync(UpdateDoctorDto updateDoctorDto);
+        Task DeleteDoctorAsync(int id);
+    }
+}
